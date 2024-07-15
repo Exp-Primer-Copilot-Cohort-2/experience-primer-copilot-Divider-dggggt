a0935acb250aecbcdd47ffd7b3bfa3a23ae66da4
caoboqian <bcao4832@uni.sydney.edu.au>
2024-07-15 15:31:51 +0800

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillsMember() {
+  const member = {
+    name: 'Divya Gogula',
+    role: 'Software Engineer',
+    avatarSrc: 'https://avatars.githubusercontent.com/u/13942672?v=4',
+    social: [
+      {
+        name: 'GitHub',
+        url: '
